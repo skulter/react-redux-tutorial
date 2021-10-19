e52472a1a548d0b3f74d75756464fe5f0c99627b
김진호 <skulter@naver.com>
2021-10-19 14:40:40 +0900

    리덕스공부

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './modules';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWitthDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWitthDevTools);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
