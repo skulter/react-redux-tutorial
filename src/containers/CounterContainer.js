da69f4fe2ddc18b87bdfcd0af5d6a6488b47bdf8
김진호 <skulter@naver.com>
2021-10-20 09:31:04 +0900

    useSelector로 상태 조회

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -3,11 +3,19 @@ import Counter from '../components/Counter';
 import { connect } from 'react-redux';
 import { increase, decrease } from '../modules/counter';
 // import { bindActionCreators } from 'redux';
+import { useSelector } from 'react-redux';
 
-const CounterContainer = ({ number, increase, decrease }) => {
-  return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
-  );
+//#region ################ 일반적인 connect 사용 ###################
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return (
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+//   );
+// };
+//#endregion
+
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  return <Counter number={number} />;
 };
 
 //#region ############# 1. mapStateToProps, mapDispatchToProps 를 사용한 방법 #############
@@ -57,14 +65,16 @@ const CounterContainer = ({ number, increase, decrease }) => {
 //#endregion
 
 //#region ############# 4. mapDispatchToProps에 해당하는 파라미터 함수 형태가 아닌 액션 생성 함수로 이루어진 객체 형태로 넣어주는 방법 #############
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  },
-)(CounterContainer);
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   {
+//     increase,
+//     decrease,
+//   },
+// )(CounterContainer);
 // 위와 같이 두 번째 파라미터를 아예 객체 형태로 넣어주면 connect 함수가 내부적으로 bindActionCreators 작업을 도와준다.
 //#endregion
+
+export default CounterContainer;
